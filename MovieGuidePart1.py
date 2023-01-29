3702ae569a149479c6007148d41ed7e460f473bf
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-01-28 20:21:21 -0800

    Lists

--- MovieGuidePart1.py
+++ MovieGuidePart1.py
@@ -1,1 +1,54 @@
+# Torrance Prince CIS261 MovieGuidePart1
 
+def display_menu():
+    print("The Movie List Program")
+    print()
+    print("COMMAND MENU")
+    print("list - List all movies")
+    print("add - Add a movie")
+    print("del - Delete a movie")
+    print("exit - Exit the program")
+    print()
+
+def list(movie_list):
+    for i, movie in enumerate(movie_list, start=1):
+        print(f"{i}. {movie}")
+    print()
+
+def add(movie_list):
+    movie = input("Name: ")
+    movie_list.append(movie)
+    print(f"{movie} was added. \n")
+
+def delete(movie_list):
+    number = int(input("Number: "))
+    if number < 1 or number > len(movie_list):
+        print("Invalid movie number.\n")
+    else:
+        movie = movie_list.pop(number-1)
+        print(f"{movie} was deleted. \n")
+
+def main():
+    movie_list = ["Billy Madison",
+                  "Men in Black",
+                  "The Grand Budapest Hotel"]
+
+    display_menu()
+
+    while True:
+        command = input("Command: ")
+        if command.lower() == "list":
+            list(movie_list)
+        elif command.lower() == "add":
+            add(movie_list)
+        elif command.lower() == "del":
+            delete(movie_list)
+        elif command.lower() == "exit":
+            break
+        else:
+            print("Not a valid value. Please try again.\n")
+
+    print("Bye!")
+
+if __name__ == "__main__":
+    main()
